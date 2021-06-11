a554fc191ac10f5d9725df9ee01079385d81f4ac
simon <57360685+sexysim0n@users.noreply.github.com>
2021-06-11 11:19:08 +0200

    Fixes
    gotta fix thing yk

--- out/artifacts/DriveSpaceTool_jar2/frickingexecute.bat
+++
@@ -1,2 +0,0 @@
-java --module-path "C:\Users\simon\data\HTL\sew\openjfx-11.0.2_windows-x64_bin-sdk\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar ./DriveSpaceTool.jar MainClass
-pause
--- out/artifacts/DriveSpaceTool_jar2/windows/frickingexecute.bat
+++ out/artifacts/DriveSpaceTool_jar2/windows/frickingexecute.bat
@@ -1,1 +1,1 @@
-start "" "javaw" --module-path "C:\Users\simon\data\HTL\sew\openjfx-11.0.2_windows-x64_bin-sdk\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar ./DriveSpaceTool.jar MainClass
+start "" "javaw" --module-path ".\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar ./DriveSpaceTool.jar MainClass
